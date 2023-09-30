23ce52b7ac5d58db39022fa931a2d025d7910b20
Valeriya Lukomskaya <valerialukomskaa@gmail.com>
2023-09-30 16:18:52 +0300

    project complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return   <nav className="black">
+    <div className="nav-wrapper">
+      <a href="!#" className="brand-logo">Voron Shop</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">repo</a></li>
+      </ul>
+    </div>
+  </nav>
+}
+
+export{Header}
